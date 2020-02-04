d57dd8de26f34b70f9854d5a86218adf6344264f
monguis <jccnnetwork@gmail.com>
2020-02-04 13:36:22 -0700

    connected database to server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
 
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
