815b1ebb840d43f52f0ed164a7ce84cb8dd404a0
deebs <hereisdanbrown@gmail.com>
2020-02-05 15:18:44 -0700

    Added navbar shit

--- routes/html/about.js
+++ routes/html/about.js
@@ -1,8 +1,16 @@
 const path = require("path");
 const router = require("express").Router();
 
+var renderObj = {
+  pageTitle: "About",
+  h1Title: "About",
+  subtitle: "Whatcha' buyin'?",
+  about: true
+}
+
+
 router.get('/', function (req, res) {
-  res.render("index", {pageTitle: "About", youAreUsingPug: true})
+  res.render("index", renderObj);
 })
 
 module.exports = router;
