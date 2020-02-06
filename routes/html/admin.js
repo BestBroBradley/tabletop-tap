3572750db3fb740a1dd9d75cae2f091f9c958af3
monguis <jccnnetwork@gmail.com>
2020-02-06 16:47:00 -0700

    fixed middleware and andded more functionallity

--- routes/html/admin.js
+++ routes/html/admin.js
@@ -1,17 +1,28 @@
 const path = require("path");
 const router = require("express").Router();
+const db = require("../../models");
+isAuthenticated = require("../../config/Middleware/isAuthenticated")
 
-router.get('/', function (req, res) { 
-  res.render("admin", {pageTitle: "Admin",user:req.user, youAreUsingPug: true})
+router.get('/', function (req, res) {
+
+  db.Users.findAll().then(data => {
+    if (data == false) {
+      return res.render("adminSignUp", {
+        pageTitle: "Admin Sign Up", youAreUsingPug: true
+      })
+    } 
+      res.redirect(403,'/html/admin/signedUp')
+  })
 })
 
-router.get('/signup', function (req, res) { 
-  res.render("adminSignUp", {
-    pageTitle: "Admin Sign Up", youAreUsingPug: true
-  })
+router.get('/signedUp',isAuthenticated, function (req, res) {
+  res.render("admin", { pageTitle: "Admin", youAreUsingPug: true })
 })
 
 module.exports = router;
 
 
 // Don't forget to inject the filename in the pug file
+// .catch(()=>{
+  
+// })
