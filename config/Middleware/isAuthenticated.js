8d0614b7c4fdcb08994a0fd2985bcce7981b1a41
monguis <jccnnetwork@gmail.com>
2020-02-05 21:51:17 -0700

    added pug if statement for authentication rendering

--- config/Middleware/isAuthenticated.js
+++ config/Middleware/isAuthenticated.js
@@ -4,11 +4,11 @@ module.exports = function (requiredTier, route) { //it receives a requireTier an
     // If the user is logged in, continue with the request to the restricted route
     if (req.user) {
       if (req.user.tier === requiredTier) {
-        console.log("cool beans")
         return next();
       }
 
       // If the user isn't logged in, redirect them to the login page or to the received route
+      console.log(route)
       if (route) {
         return res.redirect(route);
       }
