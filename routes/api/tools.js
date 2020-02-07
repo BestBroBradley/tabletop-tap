90405d6b7dee7f819a4fae02ae774b058f725582
deep <hereisdanbrown@gmail.com>
2020-02-07 08:23:50 -0700

    I figured out why the tools didn't get added

---
+++ routes/api/tools.js
@@ -0,0 +1,13 @@
+const intKeys = ["min_time", "max_time", "min_players", "max_players"];
+const floatKeys = ["rating", "abv", "price", "id"];
+function toNum (data) {
+  for (let key in data) {
+    if (intKeys.includes(key)) {
+        data[key] = parseInt(data[key]);
+    } else if (floatKeys.includes(key)) {
+        data[key] = parseFloat(data[key]);
+    }
+  }
+  return data
+};
+module.exports = toNum;
