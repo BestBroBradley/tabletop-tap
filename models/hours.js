1857febb58efc50465f726b76d7a11b5e09391a7
monguis <jccnnetwork@gmail.com>
2020-02-07 10:40:58 -0700

    changed hours model

--- models/hours.js
+++ models/hours.js
@@ -6,7 +6,7 @@ module.exports = function (sequelize, DataTypes) {
             unique: true,
             validate:{
                 dayofTheweek(value) {
-                    const days= ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+                    const days= ["mon","tues","weds","thurs","fri","sat","sun"];
                     if(days.indexOf(value.toLowerCase())=== -1){
                         throw new Error("has to be a day of the week");
                     }
@@ -26,9 +26,9 @@ module.exports = function (sequelize, DataTypes) {
         close_time: {
             type: DataTypes.DECIMAL(4, 2),
             allowNull: false,
-            defaultValue: 24
+            defaultValue: 21
             , validate: {
-                max: 24,                  // only allow values <= 23.59
+                max: 30,                  // only allow values <= 23.59
                 min: 0.01,
                 closeTime(value) {
                     if(parseInt(value) <= parseInt(this.open_time)){
@@ -36,6 +36,11 @@ module.exports = function (sequelize, DataTypes) {
                     }
                   }
             }
+        },
+        closed_day:{
+            type: DataTypes.BOOLEAN,
+            defaultVAlue:false,
+            allowNull:false
         }
     })
     return Hours
