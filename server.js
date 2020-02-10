fd6988531bba3251cb663ed6dfb48e7da7707721
deep <hereisdanbrown@gmail.com>
2020-02-09 20:27:03 -0700

    Added path.join(__dirname, to our middleware express.static

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static("client/public"));
+app.use(express.static(path.join(__dirname, "client/public")));
 
 app.use(routes)
 
