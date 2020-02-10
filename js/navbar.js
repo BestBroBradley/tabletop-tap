4f502315afb3e36b53484d2605817a3d0075e03e
monguis <jccnnetwork@gmail.com>
2020-02-09 22:44:45 -0700

    trying serve static

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
-      $(".navbar-menu").toggleClass("is-active");
-
-  });
-});
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-process.env.PWD = process.cwd();
 const express = require("express");
 const session = require("express-session");
 const passport = require("./config/passport");
@@ -8,6 +7,7 @@ const PORT = process.env.PORT || 8080;
 const pug = require("pug");
 const db = require("./models");
 const path = require("path");
+var serveStatic = require('serve-static')
 require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
@@ -19,35 +19,33 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(serveStatic(path.join(__dirname+"/client/public")));
 
 app.use(routes)
 
 app.set("view engine", "pug");
 app.set("views", "./views");
 
-db.sequelize.sync({ force: false,logging : false }).then(function() {
-  
+db.sequelize.sync({ force: false }).then(function() {
+
   // db.Users.create({
-    //   login:"test",
-    //   password:"Password1",
-    //   email:"test@gmail.com",
-    //   tier:"ADMIN"
-    // });
-    
-    // app.get("api/admin/login",{login:"test",
-    // password:"Password1",})
-    // db.Users.create({
-      //   login:"test2",
-      //   password:"Password1",
-      //   email:"test1@gmail.com",
-      //   tier:"employee"
-      // })
-      
-      app.listen(PORT, function() {
-        app.use(express.static(__dirname+"/client/public"));
+  //   login:"test",
+  //   password:"Password1",
+  //   email:"test@gmail.com",
+  //   tier:"ADMIN"
+  // });
+
+  // app.get("api/admin/login",{login:"test",
+  // password:"Password1",})
+  // db.Users.create({
+  //   login:"test2",
+  //   password:"Password1",
+  //   email:"test1@gmail.com",
+  //   tier:"employee"
+  // })
+
+  app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-    console.log("allo")
-    console.log(process.env.PWD+"/client/public")
   });
 });
 
