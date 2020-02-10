7e7c7bd2070d8eeb21c95b3590042a80a84d6388
monguis <jccnnetwork@gmail.com>
2020-02-09 22:35:38 -0700

    gebugging stuff atempt 5

---
+++ js/games.js
@@ -0,0 +1,31 @@
+// .ready method runs the JavaScript as soon as the page's DOM becomes safe to manipulate
+$(document).ready(function() {
+  $(".view-more, .delete").on("click", function(e) {
+    console.log('clicked')
+    console.log($(this).attr("id"))
+    let id = $(this).attr("id");
+    $(`#${id}.modal`).toggle();
+  })
+                                
+                                
+  $("#searchGames").on("submit", function (evt) {
+    evt.preventDefault()
+    // $.get("/api/games/search/" + $("#query").val().trim(), function (data) {
+    //   // console.log(data)
+    //   console.log(data)
+    if($("#query").val().trim()){
+    document.location = '/html/games/search/'+ $("#query").val().trim()
+  }
+      // $.ajax({
+      //   url : "http://localhost:8080/html/games/",
+      //   method:"get"
+      // })
+    // })
+    // .then((res)=>{
+    //   console.log(res)
+    //   // location.reload();
+    // }).catch(err => {
+    //   console.log(err)}
+    // });
+  })
+})
