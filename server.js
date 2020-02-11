23a3416cae61a23e2aaef2bc0a36ad5aedae98df
Allison Bradley <allison.bradley@outlook.com>
2020-02-10 19:57:30 -0700

    Stopped resetting of database

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("public"));
 
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
@@ -23,24 +22,17 @@ app.use(express.static(path.join(__dirname, "client/Public")));
 
 app.use(routes)
 
-// all environments
-// app.use(express.favicon());
-// app.use(express.logger('dev'));
-// app.use(express.bodyParser());
-// app.use(express.methodOverride());
-// app.use(express.static(path.join(__dirname, 'public')));
-
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
 
-  db.Users.create({
-    login:"test",
-    password:"Password1",
-    email:"test@gmail.com",
-    tier:"ADMIN"
-  });
+  // db.Users.create({
+  //   login:"test",
+  //   password:"Password1",
+  //   email:"test@gmail.com",
+  //   tier:"ADMIN"
+  // });
 
   // app.get("api/admin/login",{login:"test",
   // password:"Password1",})
