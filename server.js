9fc88ddccbc28c7d3b350b7cc1d0ed6990de837e
deep <hereisdanbrown@gmail.com>
2020-02-13 14:11:39 -0700

    blah

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
